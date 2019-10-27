71d99d514e63d328c81639e8167feda4bb4f2b8b
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-27 13:02:23 -0400

    TV Container

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TVPresenter from './TVPresenter';
+import { tvApi } from '../../api';
 
 class TVContainer extends Component {
 	state = {
@@ -9,6 +10,25 @@ class TVContainer extends Component {
 		loading: true,
 		error: null
 	};
+
+	async componentDidMount() {
+		try {
+			const {
+				data: { results: topRated }
+			} = await tvApi.topRated();
+			const {
+				data: { results: popular }
+			} = await tvApi.popular();
+			const {
+				data: { results: airingToday }
+			} = await tvApi.airingToday();
+			this.setState({ topRated, popular, airingToday });
+		} catch {
+		} finally {
+			this.setState({ loading: false });
+		}
+	}
+
 	render() {
 		const { topRated, popular, airingToday, loading, error } = this.state;
 		return (
