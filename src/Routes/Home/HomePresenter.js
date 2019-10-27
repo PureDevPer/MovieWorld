4fbf847cc69e864415ff10168380c2d953962d4d
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-27 15:31:04 -0400

    Error Message

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Section from '../../Components/Section';
 import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
 
 const Container = styled.div`
 	padding: 0px 20px;
@@ -21,19 +22,20 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
 				</Section>
 			)}
 			{upcoming && upcoming.length > 0 && (
-				<Section title="Now Playing">
+				<Section title="Upcoming Movies">
 					{upcoming.map(movie => (
 						<span key={movie.id}>{movie.title}</span>
 					))}
 				</Section>
 			)}
 			{popular && popular.length > 0 && (
-				<Section title="Now Playing">
+				<Section title="Popular Movies">
 					{popular.map(movie => (
 						<span key={movie.id}>{movie.title}</span>
 					))}
 				</Section>
 			)}
+			{error && <Message color="#e74c3c" text={error} />}
 		</Container>
 	);
 
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from '../../Components/Loader';
 import Section from '../../Components/Section';
+import Message from '../../Components/Message';
 
 const Container = styled.div`
 	padding: 0px 20px;
@@ -52,6 +53,13 @@ const SearchPresenter = ({
 						{tvResults.map(tvShow => (
 							<span key={tvShow.id}>{tvShow.name}</span>
 						))}
+						{error && <Message color="#e74c3c" text={error} />}
+						{tvResults &&
+							movieResults &&
+							tvResults === 0 &&
+							movieResults === 0 && (
+								<Message color="#95a5a6" text="Nothing found" />
+							)}
 					</Section>
 				)}
 			</>
