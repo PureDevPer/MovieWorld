b814f68dc8f87583b0766c622592b624c83acadd
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-27 13:27:29 -0400

    Set up Presenter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
-	Switch,
-	Redirect
+	Redirect,
+	Switch
 } from 'react-router-dom';
-import Header from './Header';
 import Home from '../Routes/Home';
-import Search from '../Routes/Search';
 import TV from '../Routes/TV';
+import Header from './Header';
+import Search from '../Routes/Search';
 import Detail from '../Routes/Detail';
 
 export default () => (
@@ -16,9 +16,9 @@ export default () => (
 		<>
 			<Header />
 			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route path="/search" component={Search} />
+				<Route path="/" exact component={Home} />
 				<Route path="/tv" component={TV} />
+				<Route path="/search" component={Search} />
 				<Route path="/movie/:id" component={Detail} />
 				<Route path="/show/:id" component={Detail} />
 				<Redirect from="*" to="/" />
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,3 +1,16 @@
-import HomeContainer from './HomeContainer';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export default HomeContainer;
+const HomePresenter = ({ nowPlaying, popular, upcomfing, loading, error }) =>
+	null;
+
+HomePresenter.propTypes = {
+	nowPlaying: PropTypes.array,
+	popular: PropTypes.array,
+	upcomfing: PropTypes.array,
+	loading: PropTypes.bool.isRequired,
+	error: PropTypes.string
+};
+
+export default HomePresenter;
