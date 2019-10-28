1d1c872b9ee97807ca21bf448b8caec3a862790c
PureDevPer <wooseok.kim1207@gmail.com>
2019-10-28 14:28:26 -0400

    Updates

---
+++ precache-manifest.f1bd0a1911ef43d41237d517135fd1c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dafb0a4b1e85bb13fa7bf065563da8",
+    "url": "/movieworld/index.html"
+  },
+  {
+    "revision": "b7f4e1f18ad86125e267",
+    "url": "/movieworld/static/js/2.46243a78.chunk.js"
+  },
+  {
+    "revision": "e3686d2a4077149bd42d",
+    "url": "/movieworld/static/js/main.4790eac4.chunk.js"
+  },
+  {
+    "revision": "68d7ce75acd730b2e45f",
+    "url": "/movieworld/static/js/runtime-main.7d0a39ec.js"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/movieworld/static/media/IMDb.f7f2904f.png"
+  },
+  {
+    "revision": "f17b0a0955a85d7dfb3fc359f2ccfbfb",
+    "url": "/movieworld/static/media/no_poster.f17b0a09.png"
+  }
+]);
